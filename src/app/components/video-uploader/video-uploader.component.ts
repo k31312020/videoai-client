f5aae1877c8fc162b17e1c9ba115eeb3c7a9fb37
Kuenzang Gyeltshen <kuenzngg@gmail.com>
2024-06-10 17:51:46 +1000

    add header

--- src/app/components/video-uploader/video-uploader.component.ts
+++ src/app/components/video-uploader/video-uploader.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { DropzoneCdkModule } from '@ngx-dropzone/cdk';
-import { DropzoneMaterialModule } from '@ngx-dropzone/material';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
@@ -10,8 +8,6 @@ import {MatIconModule} from '@angular/material/icon';
   standalone: true,
   imports: [
     MatFormFieldModule,
-    DropzoneCdkModule,
-    DropzoneMaterialModule,
     MatButtonModule,
     MatIconModule
   ],
